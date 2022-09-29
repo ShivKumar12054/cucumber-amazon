ab84ae72acbc1ce747eb360437de327cf93ab91f
Shiva Kumar M <shivakumar12054@gmail.com>
2022-09-29 07:00:49 +0530

    cucumber framework

---
+++ src/test/java/stepDefinition/exampleFeature.java
@@ -0,0 +1,28 @@
+package stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class exampleFeature {
+    @Given("given some")
+    public void givenSome() {
+    }
+
+    @When("Whenever its needed")
+    public void wheneverItsNeeded() {
+    }
+
+    @Then("then at some")
+    public void thenAtSome() {  
+    }
+
+    @And("after click")
+    public void afterClick() {
+    }
+
+    @Then("Ok success")
+    public void okSuccess() {
+    }
+}
---
+++ src/test/resources/features/example.feature
@@ -0,0 +1,7 @@
+Feature: someFeature
+  Scenario: scanerioecample
+    Given given some
+    When Whenever its needed
+    Then then at some
+    And after click
+    Then Ok success
